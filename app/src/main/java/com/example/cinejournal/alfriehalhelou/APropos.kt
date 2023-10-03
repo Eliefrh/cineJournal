8ae54870b1d75668236f1ac46cb6cfcf8047b45c
KhalilAlhelou <64221881+KhalilAlhelou@users.noreply.github.com>
2023-10-03 01:20:33 -0400

    commencer à travailler sur les fonctionnements de la menu

---
+++ app/src/main/java/com/example/cinejournal/alfriehalhelou/APropos.kt
@@ -0,0 +1,14 @@
+package com.example.cinejournal.alfriehalhelou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class APropos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apropos)
+
+
+        //supportActionBar?.title = getString(R.string.activite_apropos)
+    }
+}
