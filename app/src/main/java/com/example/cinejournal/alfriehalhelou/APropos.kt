446e524afae272d4e0a87af6b254416003c122a2
KhalilAlhelou <64221881+KhalilAlhelou@users.noreply.github.com>
2023-10-03 22:07:06 -0400

    ajouter page à propos et l'acceder par rapport à la main acitvity

--- app/src/main/java/com/example/cinejournal/alfriehalhelou/APropos.kt
+++ app/src/main/java/com/example/cinejournal/alfriehalhelou/APropos.kt
@@ -2,13 +2,15 @@ package com.example.cinejournal.alfriehalhelou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+
 
 class APropos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_apropos)
+        setContentView(R.layout.activite_apropos)
 
-
-        //supportActionBar?.title = getString(R.string.activite_apropos)
     }
 }
--- app/src/main/java/com/example/cinejournal/alfriehalhelou/ListeDeFilms.kt
+++ app/src/main/java/com/example/cinejournal/alfriehalhelou/ListeDeFilms.kt
@@ -1,18 +1,27 @@
 package com.example.cinejournal.alfriehalhelou
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class ListeDeFilms : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.liste_film)
+        setContentView(R.layout.activite_liste_film)
 
         var toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -23,7 +32,10 @@ class ListeDeFilms : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.aPropos -> {
-
+                Log.d("MonTag", "Clic sur A Propos")
+                val intent = Intent(this, APropos::class.java);
+                startActivity(intent)
+                return true
             }
             R.id.trouverUnFilm -> {
 
@@ -31,7 +43,13 @@ class ListeDeFilms : AppCompatActivity() {
             R.id.toutSupprimer -> {
 
             }
-            R.id.tirerPar -> {
+            R.id.titre -> {
+
+            }
+            R.id.note -> {
+
+            }
+            R.id.annee -> {
 
             }
 
