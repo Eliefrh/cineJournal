5a13b4fb2d574ed645346d26e135450219c39e06
Eliefrh <2067396@bdeb.qc.ca>
2023-10-06 09:51:50 -0400

    changement de theme et ajout du bouton retour
    faire un bouton avec la meme couleur du toolbar
    
    le bouton fait un retour au page principle

--- app/src/main/java/com/example/cinejournal/alfriehalhelou/APropos.kt
+++ app/src/main/java/com/example/cinejournal/alfriehalhelou/APropos.kt
@@ -1,7 +1,10 @@
 package com.example.cinejournal.alfriehalhelou
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
@@ -12,7 +15,12 @@ class APropos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activite_apropos)
 
+        val retour: Button = findViewById(R.id.retour)
 
+        retour.setOnClickListener{
+           val intent = Intent(this, ListeDeFilms::class.java)
+            startActivity(intent)
+        }
 
     }
 }
